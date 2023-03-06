26f22fbb94e0fcce86e7dae54d886588db5a7b89
Tatsiana <tptushko@my.centennialcollege.ca>
2023-03-05 22:45:56 -0500

    UI and removed some bugs

--- Assets/[Scripts]/ArrivalProcess.cs
+++ Assets/[Scripts]/ArrivalProcess.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ArrivalProcess : MonoBehaviour
 {
@@ -13,33 +14,59 @@ public class ArrivalProcess : MonoBehaviour
 
     public bool isSimulationRunning = true;
     public int timeScaling = 5;
-
+    public TMP_Text arrivalTxt;
+    public TMP_Text numberOfCustomers;
+    public float speed = 1f;
+    public GameObject initWaitingPoint;
+    private GameObject customerGO;
+    GameObject ATM;
     public List<GameObject> queue = new List<GameObject>();
+    public List<GameObject> waypoints = new List<GameObject>();
+    private float nextArrivalTimeInSec;
+   
 
 
     // Start is called before the first frame update
     void Start()
     {
+        ATM = GameObject.FindGameObjectWithTag("ATM");
         arrivalRateInCustomersPerMin = 1 / interArrivalTimeInMin;
         StartCoroutine(GenerateArrivals());
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        nextArrivalTimeInSec -= Time.deltaTime;
+        arrivalTxt.text = nextArrivalTimeInSec.ToString("0.0");
+        if (queue.Count > 0)
+        { 
+            numberOfCustomers.text = (queue.Count - 1).ToString();
 
+                if (customerGO != queue[0])
+                {                                 
+                    customerGO.transform.position = Vector3.MoveTowards(customerGO.transform.position, initWaitingPoint.transform.position, speed * Time.deltaTime);
+                } 
+       
+        }
+
+        
+            
     }
 
     IEnumerator GenerateArrivals()
     {
         while (isSimulationRunning)
         {
-            GameObject customerGO = Instantiate(customerPrefab, customerSpawnPlace.transform.position, Quaternion.identity);
+            customerGO = Instantiate(customerPrefab, customerSpawnPlace.transform.position, Quaternion.identity);
+           
 
             queue.Add(customerGO);
+            
 
             float nextArrivalTimeInMin = Utilities.GetExpDistValue(arrivalRateInCustomersPerMin);
-            float nextArrivalTimeInSec = nextArrivalTimeInMin * 60f/ timeScaling;
+             nextArrivalTimeInSec = nextArrivalTimeInMin * 60f/ timeScaling;
             print("Scaled_NextArrivalTimeInSec=" + nextArrivalTimeInSec);
             yield return new WaitForSeconds(nextArrivalTimeInSec);
         }
--- Assets/[Scripts]/Service.cs
+++ Assets/[Scripts]/Service.cs
@@ -1,28 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Service : MonoBehaviour
 {
-   // public GameObject customerGO = null;
+    // public GameObject customerGO = null;
 
     public float ServiceTimeInMin = 3f; // 1/Mu
     public float ServiceRateInCustomersPerMin; // Mu  (0.3333 -> 1/3)
     public float speed = 1f;
 
-    public int timeScaling = 5; 
+    public int timeScaling = 5;
     private bool isSimulationRunning;
     private float nextServiceTimeInSec;
-    private float currentServiceTime;
+    private float currentServiceTime =0f;
     public GameObject currentCustomer = null;
     private GameObject exitCustomer;
     private bool isInService = false;
     private bool isServiced = false;
+  
+
+    public TMP_Text serviceTxt;
 
 
     List<GameObject> queue; // = new List<GameObject>();
     GameObject ATM, exit;
-    
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -35,44 +39,46 @@ public class Service : MonoBehaviour
 
     }
     private void Start()
-    {
-        float nextServiceTimeInMin = Utilities.GetExpDistValue(ServiceRateInCustomersPerMin); //mu
-        nextServiceTimeInSec = nextServiceTimeInMin * 60f;
-        currentServiceTime = nextServiceTimeInSec / timeScaling;
-        StartCoroutine(GenerateServiceTimes());
+    {        
+        GenerateServiceTimes();
+        //  StartCoroutine(GenerateServiceTimes());
 
     }
 
     void Update()
     { //if first Customer in line exist move it towards ATM
-        if(currentCustomer != null)
-        {
+        if (currentCustomer != null)
+        {            
+
             currentCustomer.transform.position = Vector3.MoveTowards(currentCustomer.transform.position, ATM.transform.position, speed * Time.deltaTime);
 
             //start countdown service time when customer reaches ATM
             if (currentCustomer.transform.position == ATM.transform.position)
             {
+                
                 if (isInService)
                 {
-                        if (currentServiceTime > 0)
-                        {
-                            currentServiceTime -= Time.deltaTime;
-                        }
-                        else   //when service time is run out current customer becomes exiting customer
-                        {
-                            print("currentServceTime = 0 ");
-                            isInService = false;
-                            exitCustomer = queue[0];
-                            queue.RemoveAt(0);
-                            isServiced = true;
-                            currentCustomer = null;
+                    if (currentServiceTime > 0)
+                    {
+                        currentServiceTime -= Time.deltaTime;
+                        serviceTxt.text = currentServiceTime.ToString("0.0");
+                    }
+                    else   //when service time is run out current customer becomes exiting customer
+                    {
+                        print("currentServceTime = 0 ");
+                        isInService = false;
+                        isServiced = true;
+                        exitCustomer = queue[0];
+                        queue.RemoveAt(0);
+                        currentCustomer = null;
 
-                            if (queue.Count > 0)
-                            {
-                                currentCustomer = queue[0];
-                                isInService = true;
-                            }
+                        if (queue.Count > 0)
+                        {
+                            currentCustomer = queue[0];
+                            //isInService = true;
                         }
+                        currentServiceTime = GenerateServiceTimes();
+                    }
                 }
             }
         }
@@ -80,57 +86,75 @@ public class Service : MonoBehaviour
         {
             if (queue.Count > 0)
             {
-                currentCustomer = queue[0];               
+                currentCustomer = queue[0];
             }
         }
 
         //if exiting customer is alreday serviced move it towards exit point and delete
         if (isServiced)
         {
-           
+
             if (exitCustomer != null)
             {
                 exitCustomer.transform.position = Vector3.MoveTowards(exitCustomer.transform.position, exit.transform.position, speed * Time.deltaTime);
-                if (exitCustomer.transform.position == exit.transform.position )
+                if (exitCustomer.transform.position == exit.transform.position)
                 {
-                    Destroy(exitCustomer);                    
-                    isServiced = false;                    
+                    Destroy(exitCustomer);
+                    isServiced = false;
                 }
             }
             else
             {
                 print("Exit customer = null");
-            }                                                   
+            }
         }
 
     }
 
-
-    IEnumerator GenerateServiceTimes() 
-    {
-        while (isSimulationRunning)
-        {
-           if (currentCustomer != null)
-            {                             
-                float nextServiceTimeInMin = Utilities.GetExpDistValue(ServiceRateInCustomersPerMin); //mu
+    private float GenerateServiceTimes()
+    { 
+        float nextServiceTimeInMin = Utilities.GetExpDistValue(ServiceRateInCustomersPerMin); //mu
                 nextServiceTimeInSec = nextServiceTimeInMin * 60f;
                 currentServiceTime = nextServiceTimeInSec / timeScaling;
-                print("Scaled_NextServiceTimeInSec=" + currentServiceTime);  
-                  isInService = true;     
-                yield return new WaitForSeconds(currentServiceTime);
-               
-
-            }
-            if (currentCustomer == null)
-            {
-                if (queue.Count > 0)
-                {
+                print("Scaled_NextServiceTimeInSec=" + currentServiceTime);
+                isInService = true;
+        if (currentCustomer == null)
+        {           
+           if (queue.Count > 0)
+           {
                     currentCustomer = queue[0];
-                   isInService = true;                  
-                }
-            }
-            yield return null;
-        }
-          
+           }
+         
+        }          
+       
+        return currentServiceTime;
     }
+
+    //IEnumerator GenerateServiceTimes() 
+    //{
+    //    while (isSimulationRunning)
+    //    {
+    //       if (currentCustomer != null)
+    //        {                             
+    //            float nextServiceTimeInMin = Utilities.GetExpDistValue(ServiceRateInCustomersPerMin); //mu
+    //            nextServiceTimeInSec = nextServiceTimeInMin * 60f;
+    //            currentServiceTime = nextServiceTimeInSec / timeScaling;
+    //            print("Scaled_NextServiceTimeInSec=" + currentServiceTime);  
+    //              isInService = true;     
+    //            yield return new WaitForSeconds(currentServiceTime);
+
+
+    //        }
+    //        if (currentCustomer == null)
+    //        {
+    //            if (queue.Count > 0)
+    //            {
+    //                currentCustomer = queue[0];
+    //               isInService = true;                  
+    //            }
+    //        }
+    //        yield return null;
+    //    }
+
+    //}   }
 }
